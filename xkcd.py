81fdf0a7c1cda98dc11a20d28a8bebfba040467e
John Kraus <john@johnfkraus.com>
2022-01-17 20:44:29 -0500

    read the file that contains definitions

--- xkcd.py
+++ xkcd.py
@@ -10,32 +10,39 @@ from clint.textui import puts, indent, colored
 num_words_in_pw = 4
 word_list_filename = 'wordlists/eff_large_wordlist.txt'
 
-def create_wordlist(filename, numbered_list=False, maximum_word_length=100, words_start_with="N/A", words_contain="N/A", words_end_with="N/A"):
-    temp = None
+def create_wordlist(filename, numbered_list=False, maximum_word_length=100, 
+                    contains=None, words_start_with=None,  words_end_with=None):
+    templist = None
     with open(filename) as f:
-        if words_contain == "N/A" and words_start_with == "N/A" and words_end_with == "N/A":
-            temp = [word.strip().lower()
+        if contains == None and words_start_with == None and words_end_with == None:
+            templist = [word.strip().lower()
                     for word in f if len(word) <= maximum_word_length]
-            # return words
         else: 
-            temp = [word.strip().lower() for word in f if len(word) <= maximum_word_length and word.lower().startswith(words_start_with)]
-            #return words
+            contains = contains.lower()
+            templist = [word.strip().lower() for word in f if len(word) <= maximum_word_length and contains in word.lower()]
+        
+        if numbered_list == True:
+            words2 = [word.split()[1] for word in templist]
+            return words2
+        else:
+            return templist
 
 
-    # using list comprehension to remove duplicated from list
-    words = []
-    [words.append(x) for x in temp if x not in words]
-    return words   
+    # # using list comprehension to remove duplicated from list
+    # words = []
+    # [words.append(x) for x in templist if x not in words]
+    # return words   
 
 def Sorting(lst):
     lst2 = sorted(lst, key=len)
     return lst2
 
 
-def create_xkcd_password(filename="wordlists/Collins_Scrabble_Words_2019.txt", num_words_in_password=7, numbered_list=False, contains=None, maximum_word_length=100, words_start_with="N/A"):
+def create_xkcd_password(filename="wordlists/Collins_Scrabble_Words_2019.txt", 
+    num_words_in_password=7, numbered_list=False, contains=None, maximum_word_length=100, words_start_with=None, words_end_with=None):
     with open(filename) as f:
-        words = create_wordlist(filename, numbered_list, maximum_word_length, words_start_with)
-        # words = [word.strip().lower() for word in f if len(word) <= maximum_word_length and word.startswith('Q')]
+        words = create_wordlist(filename, numbered_list,
+                                maximum_word_length, contains, words_start_with)
         word_list_length = len(words)
         possible_combinations = math.pow(word_list_length,num_words_in_password)
         entropy_bits = round(math.log(possible_combinations,2))
@@ -44,11 +51,11 @@ def create_xkcd_password(filename="wordlists/Collins_Scrabble_Words_2019.txt", n
         print("maximum word length = " + str(maximum_word_length))
         print("number of possible combinations of words = " + str(possible_combinations))
         print("password entropy bits = " + str(entropy_bits))
-        if numbered_list == True:
-            words2 = [word.split()[1] for word in words]
-            password = ' '.join(secrets.choice(words2) for i in range(num_words_in_password)).lower()
-        else:
-            password = ' '.join(secrets.choice(words) for i in range(num_words_in_password)).lower()
+        # if numbered_list == True:
+        #     words2 = [word.split()[1] for word in words]
+        #     password = ' '.join(secrets.choice(words2) for i in range(num_words_in_password)).lower()
+        # else:
+        password = ' '.join(secrets.choice(words) for i in range(num_words_in_password)).lower()
 
     return password, entropy_bits, word_list_length, possible_combinations
 
@@ -97,7 +104,7 @@ def main(args=sys.argv[1:]):
 
     parser.add_argument("--num",
                         help="number of passwords to generate",
-                        type=int, default=3)
+                        type=int, default=1)
 
     parser.add_argument("--numwords",
                         help="number of words in each password",
@@ -107,61 +114,56 @@ def main(args=sys.argv[1:]):
                         help="max number of characters in each word",
                         type=int, default=8)
 
-    parser.add_argument("-s", "--startswith",
-                        help="words in password should start with one of these letters",
-                        type=str)
+    parser.add_argument("--wordlist",
+                        help="list of words to select from",
+                        type=str, default="wordlists/Collins_Scrabble_Words_2019.txt")
 
-    parser.add_argument("-c", "--contains",
-                        help="words in password should contain one of these letters",
-                        type=str)
+    # parser.add_argument("-f",
+    #                     "--flag",
+    #                     help="Specify a flag",
+    #                     action="store_true")
 
-    parser.add_argument("-e", "--endswith",
-                        help="words in password should end with one of these letters",
-                        type=str)
-
-    parser.add_argument("-f",
-                        "--flag",
-                        help="Specify a flag",
-                        action="store_true")
-
-    parser.add_argument("--rating",
-                        help="An option with a limited range of values",
-                        choices=[1, 2, 3],
-                        type=int)
+    # parser.add_argument("--rating",
+    #                     help="An option with a limited range of values",
+    #                     choices=[1, 2, 3],
+    #                     type=int)
 
     # Allow --day and --night options, but not together.
     group = parser.add_mutually_exclusive_group()
 
-    group.add_argument("--day",
-                       help="mutually exclusive option",
-                       action="store_true")
+    group.add_argument("-s", "--startswith",
+                        help="words in password should start with one of these letters",
+                        type=str, default = None)
 
-    group.add_argument("--night",
-                       help="mutually exclusive option",
-                       action="store_true")
+    group.add_argument("-c", "--contains",
+                        help="words in password should contain one of these letters",
+                        type=str, default = "z")
+
+    group.add_argument("-e", "--endswith",
+                        help="words in password should end with one of these letters",
+                        type=str, default = None)
+
+    # group.add_argument("--day",
+    #                    help="mutually exclusive option",
+    #                    action="store_true")
+
+    # group.add_argument("--night",
+    #                    help="mutually exclusive option",
+    #                    action="store_true")
 
     args = parser.parse_args(args)
 
     print(args)
-    puts(colored.blue("Arguments"))
-    #puts(colored.green("int value: ") + str(args.int_value))
-    puts(colored.green("flag: ") + str(args.flag))
-    puts(colored.green("num: ") + str(args.num))
-    puts(colored.green("num words in password: ") + str(args.numwords))
-    puts(colored.green("max word len: ") + str(args.maxwordlen))
-    puts(colored.green("starts with: ") + str(args.startswith))
-    puts(colored.green("contains: ") + str(args.contains))
-    puts(colored.green("ends with: ") + str(args.endswith))
 
     for n in range(0, args.num):
-        wordlist = "wordlists/Collins_Scrabble_Words_2019.txt"
+        #wordlist = "wordlists/Collins_Scrabble_Words_2019.txt"
         # number_of_words_in_password = 7
         # maximum_word_length = args.maxwordlen
 
-        words_start_with = "z"
-        # password = create_xkcd_password(wordlist, number_of_words_in_password,
+        #words_start_with = "z"
+        #å password = create_xkcd_password(wordlist, number_of_words_in_password,
         #                                 maximum_word_length=maximum_word_length, words_start_with=words_start_with)
-        password = create_xkcd_password(wordlist, int(args.numwords),
+        password = create_xkcd_password(filename=args.wordlist, num_words_in_password=int(args.numwords),
                                          maximum_word_length=args.maxwordlen, contains=args.contains)
 
         print(password)
