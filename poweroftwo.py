d3586a1523336ed413da33d3ed91d96203c1aa5f
John Kraus <john@johnfkraus.com>
2022-02-17 21:32:59 -0500

    hiding experimental files

--- poweroftwo.py
+++
@@ -1,8 +0,0 @@
-import math
-
-possible_combinations = 255 # int(1024 + 1024/2)
-print((possible_combinations))
-print(bin(possible_combinations))
-print(math.log(possible_combinations, 2))
-print(math.pow(2, math.log(possible_combinations, 2)))
-
--- rotate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PASSWORD="pthree.org"
-for i in `seq 1000`; do
-    PASSWORD=`echo -n $PASSWORD | sha1sum - | awk '{print $1}'`
-done
-echo $PASSWORD
-
-
