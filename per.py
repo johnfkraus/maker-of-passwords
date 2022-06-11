f700bf348dd571a0bed75b39ec0a715bdf4feac2
John Kraus <johnkraus3@gmail.com>
2022-06-11 17:14:50 -0400

    updated mult persistance script

--- per.py
+++ per.py
@@ -1,31 +1,31 @@
-
-# numerical persistance; https://www.youtube.com/watch?v=Wim9WJeDTHQ&ab_channel=Numberphile
-
+# Compute the multiplicative persistence of an integer.
+# numerical (multiplicative) persistence; https://www.youtube.com/watch?v=Wim9WJeDTHQ&ab_channel=Numberphile
+# https://en.wikipedia.org/wiki/Persistence_of_a_number
 # run as: `python per.py 277777788888899` to get 11 steps
 
 import sys
 
+
 def per(n, steps=0):
-  if len(str(n)) == 1:
-    # print(n)
-    print("Total # of steps: ", steps)
-    retval = "Total # of steps = " + str(steps)
-    print(retval)
-    return "Total # of steps = " + str(steps)
+    if len(str(n)) == 1:
+        # print(n)
+        print("Total # of steps: ", steps)
+        retval = "Total # of steps = " + str(steps)
+        print(retval)
+        return "Total # of steps = " + str(steps)
 
-  steps += 1
-  digits = [int(i) for i in str(n)]
+    steps += 1
+    digits = [int(i) for i in str(n)]
+    print("digits = ", digits, ", len(digits) = ", len(digits))
+    result = 1
+    for j in digits:
+        result *= j
+    print("result = ", result)
 
-  result = 1
-  for j in digits:
-    result *= j
-  print(result)
-
-  per(result, steps)
+    per(result, steps)
 
 
 # print(per(778899999))
- 
 
 # print(per(277777788888899))
 '''
@@ -37,11 +37,13 @@ first, second = get_args(*sys.argv)
 print(first, second)
 '''
 
+
 # ONE PARAM
 def get_args(name='default', first=277777788888899):
     return int(first)
 
-first  = get_args(*sys.argv)
+
+first = get_args(*sys.argv)
 print("first = ", first)
 
 print(per(first))
